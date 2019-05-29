4e366fc024725b471a2ca021ebf7d0715de7448d
scaredturtle <demaram2012@yahoo.com>
2019-05-29 18:24:39 -0400

    Starting to restructure the code slightly. Will be separating it into classes soon

--- middle_earth_roguelike.py
+++ middle_earth_roguelike.py
@@ -1,24 +1,18 @@
-import tdl
 import tcod
 import sys
 
-def game():
-	sw = 80
-	sh = 60
-	player_posx = sw/2
-	player_posy = sh/2
+screen_width = 80
+screen_height = 60
+playerx = int(screen_width/2)
+playery = int(screen_height/2)
 
-	tcod.console_set_custom_font('arial10x10.png', tcod.FONT_TYPE_GRAYSCALE | tcod.FONT_LAYOUT_TCOD)
+tcod.console_set_custom_font('arial10x10.png', tcod.FONT_TYPE_GRAYSCALE | tcod.FONT_LAYOUT_TCOD)
 
-	tcod.console_init_root(sw, sh, 'Middle Earth RL', False)
+tcod.console_init_root(screen_width, screen_height, 'Middle Earth RL', False)
 
-	while not tcod.console_is_window_closed():
-		tcod.console_set_default_foreground(0, tcod.green)
+while not tcod.console_is_window_closed():
+	tcod.console_set_default_foreground(0, tcod.green)
 
-		tcod.console_put_char(0, 1, 1, '@', tcod.BKGND_NONE)
+	tcod.console_put_char(0, playerx, playery, '@', tcod.BKGND_NONE)
 
-		tcod.console_flush()
-
-if __name__ == '__main__':
-	game()
-	sys.exit
+	tcod.console_flush()
