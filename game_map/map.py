5c0652452b55fd8e9b5d0881497f020c29141564
scaredturtle <demaram2012@yahoo.com>
2019-06-01 13:08:58 -0400

    Updating an changes for work on the laptop

--- game_map/map.py
+++ game_map/map.py
@@ -7,6 +7,13 @@ class TileMap():
         if sight == None: sight = blocked
         self.sight = sight
 
+class Rect():
+    def __init__(self, x, y, w, h):
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + w
+        self.y2 = y + h
+
 class Map():
     def __init__(self, con):
         self.map_width = 60
@@ -19,10 +26,13 @@ class Map():
             for y in range(self.map_height)]
                 for x in range(self.map_width)]
 
-        self.current_map[30][22].blocked = True
-        self.current_map[30][22].sight = True
-        self.current_map[50][22].blocked = True
-        self.current_map[50][22].sight = True
+        #self.current_map[30][22].blocked = True
+        #self.current_map[30][22].sight = True
+        #self.current_map[50][22].blocked = True
+        #self.current_map[50][22].sight = True
+
+    def create_room(self, room):
+        pass
 
     def render_map(self):
         for y in range(self.map_height):
@@ -32,4 +42,3 @@ class Map():
                     tcod.console_put_char_ex(self.con, x, y, '#', self.tree_color, tcod.black)
                 else:
                     tcod.console_put_char_ex(self.con, x, y, '.', self.grass_color, tcod.black)
-
